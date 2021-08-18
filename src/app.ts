c9bd7630bce6a9f73a7715eeffcf7f554f263f43
Eugene Paitoo <eugenepaitoo@gmail.com>
2021-08-18 10:06:50 +0000

    set up MongoDb Config

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,53 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+
 import { WelcomeRoute } from "./routes/welcome.route";
 
 class App {
+  public app: express.Application;
+  public mongoUrl: string = "mongodb://localhost:27017/library";
 
-    public app: express.Application;
+  constructor() {
+    this.app = express();
+    this.setConfig();
+    this.setUpMongo();
+    this.setRoutes();
+  }
 
-    constructor() {
-        this.app = express();
-        this.setConfig();
-        this.setRoutes();
-
+  private setConfig(): void {
+    dotenv.config();
+    if (process.env.NODE_ENV === "development") {
+      this.app.use(morgan("dev"));
     }
 
-    private setConfig(): void {
-        // Allows us to receive requests with data in json format
-        this.app.use(express.json({ limit: "50mb" }));
-        // Allows us to receive requests with data in x-www-form-urlencoded format
-        this.app.use(express.urlencoded({ limit: "50mb", extended: true }));
-        // Enables Helmet 
-        this.app.use(helmet());
-        // Enables cors
-        this.app.use(cors());
-    }
+    // Allows us to receive requests with data in json format
+    this.app.use(express.json({ limit: "50mb" }));
+    // Allows us to receive requests with data in x-www-form-urlencoded format
+    this.app.use(express.urlencoded({ limit: "50mb", extended: true }));
+    // Enables Helmet
+    this.app.use(helmet());
+    // Enables cors
+    this.app.use(cors());
+  }
 
-    private setRoutes() {
-        const welcomeRoute = new WelcomeRoute();
-        this.app.use("/", welcomeRoute.router);
-    }
+  // Setup MongoDB
+  private setUpMongo(): void {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(this.mongoUrl, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    });
+  }
 
-   
-
-
+  private setRoutes(): void {
+    const welcomeRoute = new WelcomeRoute();
+    this.app.use("/", welcomeRoute.router);
+  }
 }
 
 export default new App().app;
